f1d2a6007b44d0be2a109830d5aeeb251387d5a4
Tylerhayman <128461465+Tylerhayman@users.noreply.github.com>
2023-04-28 12:06:52 +0100

    Built the JavaScript logic for scooter

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,52 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor(station,registeredUsers) {
+    this.station = station;
+    this.registeredUsers = registeredUsers;
+  }
+
+  registerUser() {
+    registeredUsers = false;
+    if (username == username && password == password) {
+      console.log("Your account has been created successfully!")
+      registeredUsers = true;
+    }
+  }
+
+  loginUser() {
+    if (username == username && password == password && registeredUsers == true) {
+      console.log("You have been logged in.")
+    } else {
+      console.log("You haven't been logged in, please try again.")
+    }
+
+  }
+
+  logoutUser() {
+    if (username == username && password == password && registeredUsers == true && loggedIn == true) {
+      console.log("You have successfully logged out of your account.")
+  } else {
+    console.log("You haven't been able to log out at this time, are you sure you have an account and that you are logged in?")
+  }
+  }
+
+
+  // Creates a scooter when the scooter amount at a station is 0, then uses logic within scooter class to create one.
+createScooter() {
+  if (Scooter.stationAmt == 0) {
+    newScooter = true
+  }
+    
+  }
+// Uses the scooter dock logic from within the scooter class.
+dockScooter() {
+  {Scooter.dock()} 
+  }
+// Uses the scooter rent logic from within the scooter class.
+rentScooter() {
+  {Scooter.rent()}  
+  }
 }
 
 module.exports = ScooterApp
